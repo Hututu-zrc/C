3836524fe439a9836ad02f5fd765f9a191cd91a6
memories-added-points <2144846840@qq.com>
2023-08-14 15:06:56 +0800

    8月14日

--- git/git/test.c
+++ git/git/test.c
@@ -1,7 +1,155 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+
+//static局部变量的用法
+//void test()
+//{
+//	static int a = 1;
+//	a++;
+//	printf("%d ", a);
+//
+//}
+//
+//int main()
+//{
+//	int i = 0;
+//	while (i < 10)
+//	{
+//		test();
+//		i++;
+//
+//	}
+//	return 0;
+//}
+
+//#define ADD(x,y) ((x)+(y))
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int c = ADD(a, b);
+//	printf("%d\n", c);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* p = &a;
+//	return 0;
+//
+//}
+//
+//struct stu
+//{
+//	char name[20];
+//	int age;
+//	char sex[10];
+//	char tele[12];
+//};
+//int main()
+//{
+//	struct stu s = { "朱润财",20,"18","1588888888" };
+//	printf("%s %d %s %s", s.name, s.age, s.sex, s.tele);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int day = 6;
+//	switch (day)
+//	{
+//		case 1:
+//		case 2:
+//		case 3:
+//		case 4:
+//		case 5:
+//			printf("weekday\n");
+//			break;
+//		case 6:
+//		case 7:
+//			printf("wekkend\n");
+//			break;
+//		default:
+//			break;
+//
+//
+//	}
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int ch = 0;
+//	while ((ch = getchar()) != EOF)
+//		putchar(ch);
+//	return 0;
+//
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char ch = "\0";
+//	while ((ch = getchar()) != EOF)
+//	{
+//		if (ch < '0' || ch > '9')//判断阿斯玛值，确定是否为数字，不是数字则跳过
+//			continue;
+//		putchar(ch);
+//	}
+//	return 0;
+//}
+
+////
+//int main()
+//{
+//	int a;
+//	scanf("%d",& a);
+//	printf("%d\n", a);
+//	return 0;
+//}
+
+//# include <stdio.h>
+//int main(void)
+//{
+//    int i;
+//    scanf("%d", &i);  //&i 表示变量 i 的地址，&是取地址符
+//    printf("i = %d\n", i);
+//    return 0;
+//}
+
+
 #include<stdio.h>
 int main()
 {
-	int a = 0;
-	printf("%d\n", a);
+	char password[20] = { 0 };
+	printf("请输入密码：");
+	scanf("%s\n", password);
+	int ch = 0;
+	while ((ch = getchar())!='\n')
+	{
+		;
+	}
+	printf("请确认密码（Y\N）：");
+	int ret = getchar();
+	if ('Y' == ret) 
+	{
+		printf("yes\n");
+
+	}
+	else
+	{
+		printf("no\n");
+	}
 	return 0;
 }
