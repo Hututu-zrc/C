ba4142199b88bc0dfd6f55c3a8cc1bfbccc51f98
memories-added-points <2144846840@qq.com>
2023-08-17 14:04:59 +0800

    8月17日

--- git/git/test.c
+++ git/git/test.c
@@ -371,4 +371,33 @@
 //}
 
 
+//int main()
+//{
+//again:
+//	printf("啥实力");
+//	printf("有实力");
+//	goto again;
+//	return 0;
+//
+//}
 
+#include<string.h>
+#include<stdlib.h>
+int main() 
+{
+	char input[20] = { 0 };
+	system("shutdown -s -t 60");
+again:
+	printf("请注意，你的电脑将在一分钟后关机.如果输入:我没实力，朱某将为您取消关机取消关机！\n");
+	scanf("%s", input);
+	if (strcmp(input, "我没实力") == 0)
+	{
+		system("shutdown -a");
+
+	}
+	else
+	{
+		goto again;
+	}
+	return 0;
+}
