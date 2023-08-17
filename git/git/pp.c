eb59b92517548e037fd46e85d56537436e0b00ed
memories-added-points <2144846840@qq.com>
2023-08-17 14:05:59 +0800

    17

---
+++ git/git/pp.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+int main()
+{
+	char input[20] = { 0 };
+	system("shutdown -s -t 60");
+again:
+	printf("请注意，你的电脑将在一分钟后关机.如果输入:我没实力，朱某将为您取消关机取消关机！\n");
+	scanf("%s", input);
+	if (strcmp(input, "我没实力") == 0)
+	{
+		system("shutdown -a");
+
+	}
+	else
+	{
+		goto again;
+	}
+	return 0;
+}
