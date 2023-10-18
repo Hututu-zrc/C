b371905f5c8aaa7a7977c76b17c417de5339e5ee
memories-added-points <2144846840@qq.com>
2023-10-18 20:33:37 +0800

    10.18

--- Do_execise/Do_execise/do_exercise.c
+++ Do_execise/Do_execise/do_exercise.c
@@ -6867,3 +6867,114 @@
 //		printf("%d\n", i-1);
 //	}
 //}
+//void print(int **s)
+//{
+//	int b = 10;
+//	*s =&b ;
+//	printf("*s=%d\n", *s);
+//}
+//int main()
+//{
+//	int a = 5;
+//	int* p = &a;
+//	*p = 25;
+//	print(&p);
+//	printf("*p=%d\n", *p);
+//	return 0;
+//}
+//void print(int s)
+//{
+//	int b = 10;
+//	s =&b ;
+//	printf("*s=%d\n", *s);
+//}
+//int main()
+//{
+//	int a = 5;
+//	int* p = NULL;
+//	print(p);
+//	return 0;
+//}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LNode {
+	int stnumber;
+	int score;
+	struct LNode* next;
+
+} LNode, * LinkList;
+
+void CreateList(LinkList &phead, int n) {
+	LinkList p = phead;
+	while (p->next) {
+		p = p->next;
+	}
+	for (int i = 0; i < n; i++) {
+		LinkList s = (LinkList)malloc(sizeof(LNode));
+		p->next = s;
+		scanf("%d %d", &s->stnumber, &s->score);
+		s->next = NULL;
+		p = p->next;
+	}
+}
+void InitList(LinkList &L) {
+	L = (LinkList)malloc(sizeof(LNode));
+	L->next = NULL;
+}
+void MergeList_L(LinkList &La, LinkList& Lb, LinkList& Lc) {
+	LinkList pa = La->next;
+	LinkList pb = Lb->next;
+	LinkList pc;
+	Lc = pc = La;
+	while (pa && pb) {
+		if (pa->stnumber <= pb->stnumber) {
+			pc->next = pa;
+			pc = pa;
+			pa = pa->next;
+
+		}
+		else {
+			pc->next = pb;
+			pc = pb;
+			pb = pb->next;
+		}
+	}
+	pc->next = pa ? pa : pb;
+}
+
+void print(LinkList  &Lc) {
+	LinkList p = Lc->next;
+	while (p != NULL) {
+		printf("%d %d\n", p->stnumber, p->score);
+		p = p->next;
+	}
+}
+
+void DestroyList(LinkList &L) {
+	LinkList pp = L;
+	while (pp) {
+		L = L->next;
+		free(pp);
+		pp = L;
+	}
+}
+
+
+
+int main() {
+	int n, m;
+	scanf("%d %d", &n, &m);
+	LinkList La, Lb, Lc;
+	InitList(La);
+	InitList(Lb);
+	CreateList(La, n);
+	CreateList(Lb, m);
+	MergeList_L(La, Lb, Lc);
+	print(Lc);
+	DestroyList(La);
+	DestroyList(Lb);
+	DestroyList(Lc);
+	return 0;
+}
