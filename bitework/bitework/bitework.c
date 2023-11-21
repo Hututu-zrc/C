cc826940ba8f7352ecf2d086f070d51a31a68c97
是回忆加了分 <2144846840@qq.com>
2023-11-21 16:51:15 +0800

    11.21

--- bitework/bitework/bitework.c
+++ bitework/bitework/bitework.c
@@ -1249,3 +1249,17 @@
 //	}
 //}
 //
+
+//Êä³öÔÂ·İ
+#include <stdio.h>
+int main()
+{
+    int year, month;
+    while (scanf("%d %d", &year, &month) != EOF)
+    {
+        int arr[] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+        arr[2] += (year % 100 != 0 && year % 4 == 0 || year % 400 == 0);
+        printf("%d\n", arr[month]);
+    }
+    return 0;
+}
