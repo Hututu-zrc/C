0b42548e4c19aa67f5ada2feae8edfbf3e095cdb
是回忆加了分 <2144846840@qq.com>
2023-11-24 19:59:45 +0800

    11.26

--- bitework/bitework/bitework.c
+++ bitework/bitework/bitework.c
@@ -1264,6 +1264,22 @@
 //    return 0;
 //}
 
-//ͨѶ¼�ı�д
+
+//int main()
+//{
+//	int aa[2][5] = { 10,9,8,7,6,5,4,3,2,1 };
+//
+//	int* ptr1 = (int*)(&aa + 1);
+//	int* ptr2 = (int*)(*(aa + 1));
+//	printf("%d,%d", *(ptr1 - 1), *(ptr2 - 1));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int arr[5] = { 1,2,3,4,5 };
+//	printf("%d\n", *(&arr + 1));
+//	return 0;
+//}
 
 
