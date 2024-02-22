67d311313ea842098f4c7b59c4d788d36a1cee1d
memories-added-points <2144846840@qq.com>
2024-02-23 00:03:57 +0800

    2.23

--- double_LinkList/double_LinkList/Double_LinkList.cpp
+++ double_LinkList/double_LinkList/Double_LinkList.cpp
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <assert.h>
 typedef struct LinkList
 {
 	int val;
@@ -11,6 +13,7 @@ typedef struct LinkList
 ListPointer LinkListCreate()
 {
 	ListPointer head = (ListPointer)malloc(sizeof(ListNode));
+	assert(head);
 	head->pre = NULL;
 	head->next = NULL;
 	head->val = 0;
@@ -19,6 +22,7 @@ ListPointer LinkListCreate()
 void LinkListAdd(int val,ListPointer& head)
 {
 	ListPointer NewNode = (ListPointer)malloc(sizeof(ListNode));
+	assert(NewNode);
 	NewNode->val = val;
 	if (head->next == NULL)
 	{
@@ -36,19 +40,67 @@ void LinkListAdd(int val,ListPointer& head)
 }
 void LinkListPrint(ListPointer& head)
 {
-	ListPointer cur = head;
+	ListPointer cur = head->next;
 	while (cur->next)
 	{
 		printf("%d ", cur->val);
 		cur = cur->next;
 	}	
 	printf("%d\n", cur->val);
-	while (cur)
+	while (cur->pre)
 	{
 		printf("%d ", cur->val);
 		cur = cur->pre;
 	}
 	
+}
+bool Listempty(ListPointer &head)
+{
+	if (head->next == NULL)
+		return true;
+	else
+		return false;
+}
+void List_pushback(ListPointer& head,int val)
+{
+	ListPointer temp = head;
+	while (temp->next != NULL)
+		temp = temp->next;
+	ListPointer newnode = (ListPointer)malloc(sizeof(ListNode));
+	assert(newnode);
+	temp->next = newnode;
+	newnode->pre = temp;
+	newnode->next = NULL;
+	newnode->val = val;
+}
+
+void List_popback(ListPointer& head)
+{
+	ListPointer temp = head;
+	while (temp->next->next != NULL)
+		temp = temp->next;
+	ListPointer tmp = temp->next;
+	tmp->pre = NULL;
+	temp->next = NULL;
+	free(tmp);
+}
+void List_pushfront(ListPointer& head, int val)
+{
+	ListPointer newnode = (ListPointer)malloc(sizeof(ListNode));
+	assert(newnode);
+	newnode->next = head->next;
+	newnode->pre = head;
+	head->next = newnode;
+	newnode->next->pre = newnode;
+	newnode->val = val;
+}
+
+void List_popfront(ListPointer&head)
+{
+	ListPointer temp = head->next;
+	head->next = head->next->next;
+	head->next->pre = head;
+	free(temp);
 }
 void DestroyLink(ListPointer &head) {//ÎªÁË±ÜÃâÄÚ´æÐ¹Â¶£¬Ã¿´Î´´½¨Á´±íÔËÐÐÍê³ÌÐòÓ¦½«´´½¨µÄÁ´±íÏú»Ù£¬±ÜÃâÄÚ´æÐ¹Â¶
 	ListPointer cur=head->next;
@@ -63,14 +115,81 @@ void DestroyLink(ListPointer &head) {//ÎªÁË±ÜÃâÄÚ´æÐ¹Â¶£¬Ã¿´Î´´½¨Á´±íÔËÐÐÍê³ÌÐòÓ
 	head->next = NULL;
 	free(head);
 }
+void List_Insert(ListPointer& head, int pos,int val)
+{
+	ListPointer temp = head;
+	while (temp != NULL && pos--)
+	{
+		temp = temp->next;
+	}
+	if (pos != -1)
+	{
+		printf("²åÈëÎ»ÖÃ·Ç·¨£¬ÇëÖØÐÂ²Ù×÷£¡£¡£¡");
+		return;
+	}
+	
+	ListPointer newnode = (ListPointer)malloc(sizeof(ListNode));
+	assert(newnode);
+	newnode->next = temp->next;
+	newnode->next->pre = newnode;
+	newnode->pre = temp;
+	temp->next = newnode;
+	newnode->val = val;
+}
+
+void List_erase(ListPointer& head, int pos)
+{
+	ListPointer temp = head;
+	while (temp != NULL && pos--)
+	{
+		temp = temp->next;
+	}
+	ListPointer tmp = temp->next;
+	temp->next = temp->next->next;
+	temp->next->pre = temp;
+	free(tmp);
+	tmp->next = NULL;
+	tmp->pre = NULL;
+}
+int List_find_pos(ListPointer& head, int val)
+{
+	ListPointer temp = head->next;
+	int pos = 0;
+	while (  temp!=NULL&& temp->val != val)
+	{
+		temp = temp->next; 
+		pos++;
+	}
+	if (temp != NULL)
+		return pos;
+	else
+		return -1;
+}
 int main()
 {
 	ListPointer head;
 	head=LinkListCreate();
 	LinkListAdd(1, head);
-	LinkListAdd(2, head);
-	LinkListAdd(3, head);
+	List_pushback(head, 3);
+	List_popback(head);
+	List_pushfront(head, 2);
+	List_popfront(head);
+	List_Insert(head, 0, 5);
+	List_Insert(head, 1, 4);
+	List_Insert(head, 2, 3);
+	List_erase(head, 0);
 	LinkListPrint(head);
+	bool ret = Listempty(head);
+	printf("\n");
+	if (ret == true)
+		printf("Á´±íÎª¿Õ£¡£¡£¡\n");
+	else
+		printf("Á´±í²»Îª¿Õ£¡£¡£¡\n");
+	int ans = List_find_pos(head, 6);
+	if (ans != -1)
+		printf(" 3µÄÎ»ÖÃÊÇ £º %d\n", ans);
+	else
+		printf("²é²»µ½£¡£¡£¡\n");
 	DestroyLink(head);
 	return 0;
 }
