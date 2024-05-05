2beed268ac9a3cdcfb056a869eddb4bcd55e7df1
memories-added-points <2144846840@qq.com>
2024-05-05 10:14:34 +0800

    5.5

--- git/git/attempt.cpp
+++ git/git/attempt.cpp
@@ -4,16 +4,16 @@
 //{
 //	char ch[] = "";
 //}
-#include <unordered_map>
-using namespace std;
-#include <string>
-#include <iostream>
-int main()
-{
-	unordered_map<string, int> m;
-	m["a"] = 1;
-	m["b"] = 2;
-	m["a"] = 10;
-	cout << m["a"] << endl;
-	return 0;
-}
+//#include <unordered_map>
+//using namespace std;
+//#include <string>
+//#include <iostream>
+//int main()
+//{
+//	unordered_map<string, int> m;
+//	m["a"] = 1;
+//	m["b"] = 2;
+//	m["a"] = 10;
+//	cout << m["a"] << endl;
+//	return 0;
+//}
