9d9267dc285ed9cce9736dccec9d80bc87592b67
memories-added-points <2144846840@qq.com>
2024-05-07 18:41:57 +0800

    5.7

--- git/git/Draft.cpp
+++ git/git/Draft.cpp
@@ -1,291 +1,410 @@
-//#include<iostream>
-//#include <string>
-//using namespace std;
-//class Solution {
-//public:
-//    bool repeatedSubstringPattern(string s) {
-//        int slow = 0;
-//        string strmin;
-//        int flag = 0;
-//        for (int fast = 1; fast < s.size(); fast++)
-//        {
-//            if (s[fast] == s[slow] && flag != 1)
-//            {
-//                strmin = s.substr(slow, fast - slow);
-//                flag = 1;
-//            }
-//            if (flag == 1)
-//            {
-//                string temp;
-//                if (fast + strmin.size() < s.size())
-//                    temp.append(s, fast, fast + strmin.size());
+////#include<iostream>
+////#include <string>
+////using namespace std;
+////class Solution {
+////public:
+////    bool repeatedSubstringPattern(string s) {
+////        int slow = 0;
+////        string strmin;
+////        int flag = 0;
+////        for (int fast = 1; fast < s.size(); fast++)
+////        {
+////            if (s[fast] == s[slow] && flag != 1)
+////            {
+////                strmin = s.substr(slow, fast - slow);
+////                flag = 1;
+////            }
+////            if (flag == 1)
+////            {
+////                string temp;
+////                if (fast + strmin.size() < s.size())
+////                    temp.append(s, fast, fast + strmin.size());
+////
+////                if (strmin.compare(temp) != 0)
+////                {
+////                    flag = 0;
+////       
+////                }
+////                else
+////                    fast += strmin.size() - 1;
+////            }
+////
+////        }
+////        if (strmin.compare(s) == 0)
+////            return false;
+////        return true;
+////    }
+////};
+////
+////class Solution {
+////public:
+////    string reverseWords(string s) {
+////        int left = 0;
+////        while (s[left] == ' ')
+////            s.erase(left, 1);
+////        int right = s.size() - 1;
+////        while (s[right] == ' ')
+////        {
+////            s.erase(right, 1);
+////            right = s.size() - 1;
+////        }
+////        int start = 0;
+////        int cur = 0;
+////        while (cur < s.size())
+////        {
+////            if (cur < s.size() && s[cur] == ' ' && s[cur + 1] != ' ')
+////            {
+////                reverse(s.begin() + start, s.begin() + cur);
+////                start = cur + 1;
+////            }
+////            else if (cur < s.size() && s[cur] == ' ' && s[cur + 1] == ' ')
+////            {
+////                s.erase(cur, 1);
+////                cur--;
+////
+////            }
+////            cur++;
+////        }
+////        reverse(s.begin() + start, s.end());
+////        reverse(s.begin(), s.end());
+////        return s;
+////    }
+////};
+////
+////int main()
+////{
+////    Solution solu;
+////    cout << solu.reverseWords("  hello world  ") << endl;
+////    return 0;
+////
+////}
 //
-//                if (strmin.compare(temp) != 0)
-//                {
-//                    flag = 0;
-//       
-//                }
-//                else
-//                    fast += strmin.size() - 1;
-//            }
 //
-//        }
-//        if (strmin.compare(s) == 0)
-//            return false;
-//        return true;
-//    }
-//};
+////#include <iostream>
+////#include <cctype>
+////#include <string>
+////using namespace std;
+////int main()
+////{
+////    string s;
+////    cin >> s;
+////    int count = 0;
+////    for (int i = 0; i < s.size(); i++)
+////    {
+////        if (isdigit(s[i]))
+////            count++;
+////    }
+////    int slow = s.size() - 1;
+////    s.resize(s.size() + count * 6 - count);
+////    for (int fast = s.size() - 1; fast >= 0; fast--)
+////    {
+////        if (slow >= 0 && fast >= 0 && isdigit(s[slow]))
+////        {
+////            s.insert(fast - 5, "number");
+////            fast -= 5;
+////            slow--;
+////        }
+////            
+////        else
+////        {
+////            s[fast]=s[slow--]  ;
+////        }
+////    }
+////    cout << s << endl;
+////    return 0;
+////}
 //
-//class Solution {
-//public:
-//    string reverseWords(string s) {
-//        int left = 0;
-//        while (s[left] == ' ')
-//            s.erase(left, 1);
-//        int right = s.size() - 1;
-//        while (s[right] == ' ')
-//        {
-//            s.erase(right, 1);
-//            right = s.size() - 1;
-//        }
-//        int start = 0;
-//        int cur = 0;
-//        while (cur < s.size())
-//        {
-//            if (cur < s.size() && s[cur] == ' ' && s[cur + 1] != ' ')
-//            {
-//                reverse(s.begin() + start, s.begin() + cur);
-//                start = cur + 1;
-//            }
-//            else if (cur < s.size() && s[cur] == ' ' && s[cur + 1] == ' ')
-//            {
-//                s.erase(cur, 1);
-//                cur--;
+////#include <iostream>
+////#include <cctype>
+////#include <string>
+////using namespace std;
+////int main()
+////{
+////    string s;
+////    cin >> s;
+////    int count = 0;
+////    for (int i = 0; i < s.size(); i++)
+////    {
+////        if (isdigit(s[i]))
+////            count++;
+////    }
+////    int slow = s.size() - 1;
+////    s.resize(s.size() + count * 6 - count);
+////    for (int fast = s.size() - 1; fast >= 0; fast--)
+////    {
+////        if (slow >= 0 && fast >= 0 && isdigit(s[slow]))
+////        {
+////            s[fast - 5] = 'n';
+////            s[fast - 4] = 'u';
+////            s[fast - 3] = 'm';
+////            s[fast - 2] = 'b';
+////            s[fast - 1] = 'e';
+////            s[fast] = 'r';
+////            fast -= 5;
+////            slow--;
+////        }
+////
+////        else
+////        {
+////            s[fast] = s[slow--];
+////        }
+////    }
+////    cout << s << endl;
+////    return 0;
+////}
 //
-//            }
-//            cur++;
-//        }
-//        reverse(s.begin() + start, s.end());
-//        reverse(s.begin(), s.end());
-//        return s;
-//    }
-//};
+////#include <iostream>
+////using namespace std;
+////#include <vector> 
+////#include <list>
+////#include <algorithm>
+////class Solution {
+////public:
+////    int removeDuplicates(vector<int>& nums) {
+////        int slow = 0;
+////        int flag = 0;
+////        for (int fast = slow + 1; fast < nums.size(); fast++)
+////        {
+////            if (nums[fast] == nums[slow] && flag != 1)
+////            {
+////                slow++;
+////                flag = 1;
+////            }
+////            else if(nums[fast] > nums[slow-1])
+////            {
+////                nums[slow++] = nums[fast];
+////            }
+////        }
+////        return slow;
+////    }
+////};
+////
+////void print(int val)
+////{
+////    cout << val << " ";
+////}
+////int main()
+////{
+////    vector<int>nums;
+////
+////    nums.push_back(0);
+////    nums.push_back(0);
+////    nums.push_back(1);
+////    nums.push_back(1);
+////    nums.push_back(1);
+////    nums.push_back(2);
+////    nums.push_back(2);
+////    nums.push_back(3);
+////    nums.push_back(3);
+////    nums.push_back(4);
+////    Solution s;
+////    int ret = s.removeDuplicates(nums);
+////    cout << ret << endl;
+////    for_each(nums.begin(), nums.end(), print);
+////    return 0;
+////}
 //
-//int main()
-//{
-//    Solution solu;
-//    cout << solu.reverseWords("  hello world  ") << endl;
-//    return 0;
-//
-//}
-
-
 //#include <iostream>
-//#include <cctype>
-//#include <string>
 //using namespace std;
-//int main()
-//{
-//    string s;
-//    cin >> s;
-//    int count = 0;
-//    for (int i = 0; i < s.size(); i++)
-//    {
-//        if (isdigit(s[i]))
-//            count++;
-//    }
-//    int slow = s.size() - 1;
-//    s.resize(s.size() + count * 6 - count);
-//    for (int fast = s.size() - 1; fast >= 0; fast--)
-//    {
-//        if (slow >= 0 && fast >= 0 && isdigit(s[slow]))
-//        {
-//            s.insert(fast - 5, "number");
-//            fast -= 5;
-//            slow--;
-//        }
-//            
-//        else
-//        {
-//            s[fast]=s[slow--]  ;
-//        }
-//    }
-//    cout << s << endl;
-//    return 0;
-//}
-
-//#include <iostream>
-//#include <cctype>
-//#include <string>
-//using namespace std;
-//int main()
-//{
-//    string s;
-//    cin >> s;
-//    int count = 0;
-//    for (int i = 0; i < s.size(); i++)
-//    {
-//        if (isdigit(s[i]))
-//            count++;
-//    }
-//    int slow = s.size() - 1;
-//    s.resize(s.size() + count * 6 - count);
-//    for (int fast = s.size() - 1; fast >= 0; fast--)
-//    {
-//        if (slow >= 0 && fast >= 0 && isdigit(s[slow]))
-//        {
-//            s[fast - 5] = 'n';
-//            s[fast - 4] = 'u';
-//            s[fast - 3] = 'm';
-//            s[fast - 2] = 'b';
-//            s[fast - 1] = 'e';
-//            s[fast] = 'r';
-//            fast -= 5;
-//            slow--;
-//        }
-//
-//        else
-//        {
-//            s[fast] = s[slow--];
-//        }
-//    }
-//    cout << s << endl;
-//    return 0;
-//}
-
-//#include <iostream>
-//using namespace std;
-//#include <vector> 
 //#include <list>
-//#include <algorithm>
-//class Solution {
+//#include <stack>
+//
+//
+//
+//struct ListNode {
+//    int val;
+//    struct ListNode *next;
+//    ListNode(int x) : val(x), next(NULL) {}
+//};
+//class PalindromeList {
 //public:
-//    int removeDuplicates(vector<int>& nums) {
-//        int slow = 0;
-//        int flag = 0;
-//        for (int fast = slow + 1; fast < nums.size(); fast++)
+//    bool chkPalindrome(ListNode* A) {
+//        // write code here
+//        if (A->next == nullptr)
+//            return true;
+//        ListNode* fast, * slow;
+//        slow = A;
+//        fast = A->next;
+//        stack<int> S;
+//        while (fast)
 //        {
-//            if (nums[fast] == nums[slow] && flag != 1)
+//            if (slow->val != fast->val)
+//                S.push(slow->val);
+//            else
 //            {
-//                slow++;
-//                flag = 1;
+//                fast = fast->next;
+//                while (fast && !S.empty())
+//                {
+//                    if (fast->val != S.top())
+//                        return false;
+//                    else
+//                        S.pop();
+//                    fast = fast->next;
+//
+//                }
+//                if (fast == nullptr && S.empty())
+//                    return true;
+//                else
+//                    return false;
+//
 //            }
-//            else if(nums[fast] > nums[slow-1])
-//            {
-//                nums[slow++] = nums[fast];
-//            }
+//            slow = slow->next;
+//            fast = fast->next;
 //        }
-//        return slow;
+//        return false;
 //    }
 //};
-//
-//void print(int val)
-//{
-//    cout << val << " ";
-//}
 //int main()
 //{
-//    vector<int>nums;
+//    ListNode* A, * cur;
+//   
+//    ListNode* B = new ListNode(1);
+//    A=cur = B;
+//   // cur = cur->next;
 //
-//    nums.push_back(0);
-//    nums.push_back(0);
-//    nums.push_back(1);
-//    nums.push_back(1);
-//    nums.push_back(1);
-//    nums.push_back(2);
-//    nums.push_back(2);
-//    nums.push_back(3);
-//    nums.push_back(3);
-//    nums.push_back(4);
-//    Solution s;
-//    int ret = s.removeDuplicates(nums);
-//    cout << ret << endl;
-//    for_each(nums.begin(), nums.end(), print);
+//    B = new ListNode(2);
+//    cur->next = B;
+//    cur = cur->next;
+//    B = new ListNode(3);
+//    cur->next = B;
+//    cur = cur->next;
+//
+//    B = new ListNode(1);
+//    cur->next = B;
+//    cur = cur->next;
+//
+//
+//    PalindromeList p;
+//    bool ret=p.chkPalindrome(A);
+//    if (ret)
+//        cout << "ture" << endl;
+//    else
+//        cout << "false" << endl;
+//    cur = A;
+//    while (cur)
+//    {
+//        ListNode* tmp = cur;
+//        cur = cur->next;
+//        delete(tmp);
+//    }
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//#include <string>
+//struct TreeNode {
+//    char val;
+//    struct TreeNode* left;
+//    struct TreeNode* right;
+//    
+//    TreeNode(char x) : val(x), left(nullptr), right(nullptr) {}
+//};
+//
+//class Solution
+//{
+//public:
+//    TreeNode* CreateTree(string& s, int &pos)
+//    {
+//        if (pos >= s.size() || s[pos] == '#') {
+//            pos++;
+//            return nullptr;
+//        }
+//
+//        TreeNode* node = new TreeNode(s[pos]);
+//        pos++;
+//        //abc##de#g##f###
+//        node->left = CreateTree(s, pos);
+//        //pos++;
+//        node->right = CreateTree(s, pos);
+//
+//        return node;
+//    }
+//    void InOrder(string &s, TreeNode* cur)
+//    {
+//        if (cur == nullptr)  return;
+//        
+//        InOrder(s, cur->left);
+//        s.push_back(cur->val);
+//        InOrder(s, cur->right);
+//    }
+//
+//};
+//int main()
+//{
+//    string s;
+//    getline(cin, s);
+//    Solution solution;
+//    int start = 0;
+//    TreeNode* head = solution.CreateTree(s,start);
+//    string ret;
+//    solution.InOrder(ret,head);
+//    for (auto& ch : ret)
+//    {
+//        cout << ch << " ";
+//    }
+//    cout << endl;
 //    return 0;
 //}
 
 #include <iostream>
 using namespace std;
-#include <list>
-#include <stack>
+#include <string>
+struct TreeNode {
+    char val;
+    struct TreeNode* left;
+    struct TreeNode* right;
 
-
-
-struct ListNode {
-    int val;
-    struct ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
+    TreeNode(char x) : val(x), left(nullptr), right(nullptr) {}
 };
-class PalindromeList {
+
+class Solution
+{
 public:
-    bool chkPalindrome(ListNode* A) {
-        // write code here
-        if (A->next == nullptr)
-            return true;
-        ListNode* fast, * slow;
-        slow = A;
-        fast = A->next;
-        stack<int> S;
-        while (fast)
+    void CreateTree(TreeNode *head,string& s, int &pos)
+    {
+        if (pos >= s.size())   return;
+        //abc##de#g##f###
+        
+        for (int i = pos; i < s.size(); i++)
         {
-            if (slow->val != fast->val)
-                S.push(slow->val);
+            if (s[pos] == '#')
+            {
+                head = nullptr;
+                return;
+            }
             else
             {
-                fast = fast->next;
-                while (fast && !S.empty())
-                {
-                    if (fast->val != S.top())
-                        return false;
-                    else
-                        S.pop();
-                    fast = fast->next;
-
-                }
-                if (fast == nullptr && S.empty())
-                    return true;
-                else
-                    return false;
-
+                head = new TreeNode(s[pos]);
+                CreateTree(head->left, s, ++pos);
+                CreateTree(head->left, s,++pos);
             }
-            slow = slow->next;
-            fast = fast->next;
         }
-        return false;
+   }
+    void InOrder(string& s, TreeNode* cur)
+    {
+        if (cur == nullptr)  return;
+
+        InOrder(s, cur->left);
+        s.push_back(cur->val);
+        InOrder(s, cur->right);
     }
+
 };
 int main()
 {
-    ListNode* A, * cur;
-   
-    ListNode* B = new ListNode(1);
-    A=cur = B;
-   // cur = cur->next;
-
-    B = new ListNode(2);
-    cur->next = B;
-    cur = cur->next;
-    B = new ListNode(3);
-    cur->next = B;
-    cur = cur->next;
-
-    B = new ListNode(1);
-    cur->next = B;
-    cur = cur->next;
-
-
-    PalindromeList p;
-    bool ret=p.chkPalindrome(A);
-    if (ret)
-        cout << "ture" << endl;
-    else
-        cout << "false" << endl;
-    cur = A;
-    while (cur)
+    string s;
+    getline(cin, s);
+    Solution solution;
+    int start = 0;
+    TreeNode* head = nullptr;
+    solution.CreateTree(head,s, start);
+    string ret;
+    solution.InOrder(ret, head);
+    for (auto& ch : ret)
     {
-        ListNode* tmp = cur;
-        cur = cur->next;
-        delete(tmp);
+        cout << ch << " ";
     }
-	return 0;
+    cout << endl;
+    return 0;
 }
